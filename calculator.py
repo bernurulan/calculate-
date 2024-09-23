734e83cdcd8257c5e1e706fd88a9cb1ef9204519
Bernurulan <bernur.ulanbekkyzy@alatoo.edu.kg>
2024-09-24 00:04:08 +0600

    calculate

---
+++ calculator.py
@@ -0,0 +1,24 @@
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+operator = input("Enter operator:")
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "/":
+    print(num1 - num2)
+else:
+    print("you are stupid")
+
+
+def calculate(num1, num2, operator):
+    if operator == "+":
+        print(num1 + num2)
+    elif operator == "-":
+        print(num1 - num2)
+
+
+calculate(3, 4, "+")
+
+
