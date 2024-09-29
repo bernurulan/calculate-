3bd56bddd30f999d27380c0e49b11512f7fabc18
Bernurulan <bernur.ulanbekkyzy@alatoo.edu.kg>
2024-09-29 17:50:09 +0600

    second commit

--- calculator.py
+++ calculator.py
@@ -1,33 +1,40 @@
-num1 = int(input("Enter first number:"))
-num2 = int(input("Enter second number:"))
-operator = input("Enter operator:")
+from os import system
+
+num1 = int(input())
+num2 = int(input())
+operator = input()
 
 if operator == "+":
+    _ = system('cls')
     print(num1 + num2)
 elif operator == "-":
+    _ = system('cls')
     print(num1 - num2)
 elif operator == "/":
+    _ = system('cls')
     print(num1 / num2)
 elif operator == "*":
+    _ = system('cls')
     print(num1 * num2)
 elif operator == "//":
+    _ = system('cls')
     print(num1 // num2)
 elif operator == "**":
+    _ = system('cls')
     print(num1 ** num2)
 elif operator == "%":
+    _ = system('cls')
     print(num1 % num2)
 
 else:
     print("you are stupid")
 
 
-def calculate(num1, num2, operator):
-    if operator == "+":
-        print(num1 + num2)
-    elif operator == "-":
-        print(num1 - num2)
 
 
-calculate(3, 4, "+")
+
+
+
+
 
 
